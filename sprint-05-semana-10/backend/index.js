8b474f679bf9c7a10c2762bab74d6f612f8c6150
ede-ch <edelinchaves.dev@gmail.com>
2024-10-04 13:48:55 -0300

    test(post/registros): Adição do teste para o endpoint POST.

--- sprint-05-semana-10/backend/index.js
+++ sprint-05-semana-10/backend/index.js
@@ -8,16 +8,7 @@ const port = 3001;
 // Middleware para processar JSON
 app.use(express.json());
 
-// Conectar ao MongoDB
-mongoose.connect('mongodb+srv://edelinsantospb:5TjB1TBADgwdgZVg@registro-api.ozljj.mongodb.net/?retryWrites=true&w=majority&appName=Registro-API')
-.then(() => {
-  console.log('Conectado ao MongoDB');
-})
-.catch(error => {
-  console.error('Erro ao conectar ao MongoDB:', error);
-});
-
-
+// Usar rotas
 app.use('/registros', registroRoutes);
 
 // Middleware de erro
@@ -26,7 +17,25 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: 'Algo deu errado!', error: err.message });
 });
 
+// Função para conectar ao MongoDB
+async function connectDB() {
+  try {
+    await mongoose.connect('mongodb+srv://edelinsantospb:5TjB1TBADgwdgZVg@registro-api.ozljj.mongodb.net/?retryWrites=true&w=majority&appName=Registro-API', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conectado ao MongoDB');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
-});
+// Iniciar o servidor se não estiver sendo executado em ambiente de teste
+if (process.env.NODE_ENV !== 'test') {
+  connectDB();
+  app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+  });
+}
+
+module.exports = { app, connectDB };
